dc42a4862fd07ca7084d5ebf91e7997299c8b082
kavita-v <54227800+kavita-v@users.noreply.github.com>
2020-01-17 23:38:03 +0530

    Update utils.py

--- tree/utils.py
+++ tree/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 
 def entropy(Y):
     """
@@ -8,8 +10,13 @@ def entropy(Y):
     Outpus:
     > Returns the entropy as a float
     """
-    
-    pass
+    counts = Y.value_counts(normalize=True)
+    prob_list = list(counts)
+    entropy = 0.0
+    for p_class in prob_list:
+        entropy+=(-(p_class)*np.log(p_class)/np.log(2))
+
+    return entropy
 
 def gini_index(Y):
     """
@@ -20,7 +27,13 @@ def gini_index(Y):
     Outpus:
     > Returns the gini index as a float
     """
-    pass
+    counts = Y.value_counts(normalize=True)
+    prob_list = list(counts)
+    gini_index = 1.0
+    for p_class in prob_list:
+        gini_index-=p_class*p_class
+    
+    return gini_index
 
 def information_gain(Y, attr):
     """
@@ -28,8 +41,19 @@ def information_gain(Y, attr):
     
     Inputs:
     > Y: pd.Series of Labels
-    > attr: attribute at which the gain should be calculated
+    > attr: pd.Series of attribute at which the gain should be calculated
     Outputs:
     > Return the information gain as a float
     """
-    pass
+    attr_counts = attr.value_counts(normalize=True)
+    attr_prob_list = list(attr_counts)
+    info_gain = entropy(Y)
+    for i in range(len(attr_prob_list)):
+        Y_attr = []
+        for j in range(len(attr)):
+            if attr_counts.keys()[i]  == attr[j]:
+                Y_attr.append(Y[j])
+        info_gain -= attr_prob_list[i]*entropy(Y_attr)
+
+    return info_gain
+
